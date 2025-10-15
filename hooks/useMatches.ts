93a1254d5d93adfda4a34549083a9c250882ac02
Mat95rix7 <ndjam71@gmail.com>
2025-10-15 18:20:01 +0200

    supprime hooks

--- hooks/useMatches.ts
+++
@@ -1,7 +0,0 @@
-import useSWR from 'swr'
-import { getLastMatches } from '../lib/api'
-
-export const useMatches = (teamId?: number) => {
-  const { data, error } = useSWR(teamId ? ['matches', teamId] : null, async () => (await getLastMatches(teamId!)).response ?? [])
-  return { matches: data, isLoading: !error && !data, isError: !!error }
-}
